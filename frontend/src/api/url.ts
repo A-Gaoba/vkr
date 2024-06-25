02a0025c25c760b5245cf9d8602105e43cdc6e86
a-goaba <abdulrahman1997abdo@gmail.com>
2024-06-25 22:27:42 +0500

    init

--- frontend/src/api/url.ts
+++ frontend/src/api/url.ts
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = 'http://localhost:80';
-export const API_BASE_URL = "http://167.71.62.124:80";
+// export const API_BASE_URL = "http://167.71.62.124:80";
+export const API_BASE_URL = "http://142.93.107.137:80";
